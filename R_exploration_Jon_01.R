716097a4ae8331177b0a9a1ce1652a3da9bfcc94
jleslie17 <jleslie17@yahoo.com>
2016-12-15 16:26:19 +0000

    Adding some box plots

--- R_exploration_Jon_01.R
+++ R_exploration_Jon_01.R
@@ -1,27 +1,37 @@
 library(tidyverse)
 
+# Read in data
 df <- read.csv("data/train.csv", header = T)
 head(df)
 str(df)
 df <- as_tibble(df)
 df
 names(df)
+
+# Poke around a bit
 cor(df$SalePrice, df$LotArea)
-cor(df)
 df %>% 
   ggplot(aes(x = log(LotArea), y = SalePrice)) +
   geom_point()
-df %>% 
-  ggplot(aes(Neighborhood, SalePrice)) +
-  stat_summary(
-    fun.y = median,
-    geom = "point"
-  ) +
-  coord_flip()
+
 df %>% 
   ggplot(aes(Neighborhood, SalePrice)) +
   geom_boxplot(aes(x = reorder(Neighborhood, 
                                SalePrice, 
                                FUN = median))
   ) +
+  coord_flip()
+
+df %>% 
+  ggplot(aes(Condition1, SalePrice)) +
+  geom_boxplot(aes(x = reorder(Condition1,
+                               SalePrice,
+                               FUN = median))) +
+  coord_flip()
+
+df %>% 
+  ggplot(aes(HouseStyle, SalePrice)) +
+  geom_boxplot(aes(x = reorder(HouseStyle,
+                               SalePrice,
+                               FUN = median))) +
   coord_flip()
